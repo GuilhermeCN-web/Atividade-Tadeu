3f030692c7b3507ec23f25fced8823e1dcf1e3d2
LukEscopeta <lfcnarcizo73@gmail.com>
2025-04-01 17:58:10 -0300

    atividades do tadeu completas e corretas

--- Ativ10.java
+++ Ativ10.java
@@ -1,27 +1,24 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Ativ10 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
         int limite = in.nextInt();
-        if (limite <= 0) {
+        if (limite > 0) {
             double soma = 0;
-            StringBuilder serie = new StringBuilder();
             for (int i = 1; i <= limite; i++) {
                 double termo = 1.0 / i;
                 if (i % 2 == 0) {
                     soma -= termo;
-                    serie.append(" - 1/").append(i);
                 } else {
                     soma += termo;
                     if (i > 1) {
-                        serie.append(" + ");
                     }
-                    serie.append("1/").append(i);
                 }
             }
-            System.out.printf("Soma: %.2f\n", soma);
-            System.out.println("Equivale a: " + serie.toString());
+            System.out.printf("%.2f\n", soma);
         }
     }
 }
--- Ativ9.java
+++ Ativ9.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Ativ9 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
         int limite = in.nextInt();
         
-        if (limite <= 0) {
-            double soma = 0;
-            StringBuilder serie = new StringBuilder();
+        if (limite > 0) {
+            double soma = 0.0;
             for (int i = 1; i <= limite; i++) {
                 soma += 1.0 / i;
-                serie.append("1/").append(i);
-                if (i < limite) {
-                    serie.append(" + ");
-                }
             }
             System.out.printf("%.2f\n", soma);
-            System.out.println(serie.toString());
         }
     }
 }
